c3856e9df1de730aab29700153444f7c021614e8
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-01-25 17:45:46 +0100

    :art: Setup src/ structure for app

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from '../styles/App';
+
+export default () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to Talesathome!
+      </Text>
+    </View>
+  );
+};
