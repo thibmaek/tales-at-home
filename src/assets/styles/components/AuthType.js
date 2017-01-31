c46e3ae4ef2afcd12baf99dba733d1b2efd8b4e1
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-01-31 22:54:15 +0100

    :fire: Removed component globalStyle

--- src/assets/styles/components/AuthType.js
+++ src/assets/styles/components/AuthType.js
@@ -1,5 +1,5 @@
  import { StyleSheet } from 'react-native';
- import { white, accentBlue, fontIsTitle, fontIsLabel } from 'src/assets/styles/vars';
+ import { globalStyle, white, accentBlue, fontIsTitle, fontIsLabel } from 'src/assets/styles/vars';
 
  export default StyleSheet.create({
    container: {
@@ -7,30 +7,32 @@
      flex: 1,
    },
    loginContainer: {
-     flexDirection: `column`, alignItems: `center`,
+     flexDirection: `column`,
      flex: 1,
+     alignItems: `center`,
    },
    loginTopContainer: {
      flexDirection: `column`,
+     flex: 1,
      alignItems: `center`,
      justifyContent: `center`,
-     flex: 1,
    },
    title: {
      marginBottom: 75,
+     ...globalStyle,
      ...fontIsTitle, fontSize: 50, fontWeight: `bold`,
-     color: white,
-     backgroundColor: `transparent`,
+     color: white, backgroundColor: `transparent`,
    },
-   textInput: {
+   inputContainer: {
      width: 280,
      marginBottom: 20,
      borderBottomColor: white, borderBottomWidth: 1, borderStyle: `solid`,
    },
    input: {
      height: 30,
-     color: white,
      marginTop: 30,
+     ...globalStyle,
+     color: white,
    },
    button: {
      width: 280,
@@ -38,17 +40,17 @@
      borderWidth: 0, borderRadius: 50,
    },
    buttonText: {
+     ...globalStyle,
      fontWeight: `bold`,
      color: accentBlue,
    },
    anonLogin: {
      marginBottom: 50,
-     borderBottomColor: white,
-     borderBottomWidth: 0.5,
-     borderStyle: `solid`,
      backgroundColor: `transparent`,
+     borderBottomColor: white, borderBottomWidth: 0.5, borderStyle: `solid`,
    },
    underlineLink: {
+     ...globalStyle,
      ...fontIsLabel, fontWeight: `normal`, fontSize: 11,
      color: white,
    },
--- src/components/AuthType.js
+++ src/components/AuthType.js
@@ -5,39 +5,34 @@ import LinearGradient from 'react-native-linear-gradient';
 
 import Button from 'apsl-react-native-button';
 
-import globalStyle from 'src/assets/styles/GlobalStyle.js';
 import s from 'src/assets/styles/components/AuthType';
 
 const AuthType = ({ action, title }) => (
   <LinearGradient colors={[`#008EFF`, `#00C8FF`]}  style={s.container}>
     <View style={s.loginContainer}>
       <View style={s.loginTopContainer}>
-          <Text style={[globalStyle, s.title]}>{title}</Text>
-          <View style={s.textInput}>
-            <TextInput
-              style={[globalStyle, s.input]}
-              placeholder='Wat is je e-mail adres?'
-              placeholderTextColor={white}
-            />
-          </View>
-          <View style={s.textInput}>
-            <TextInput
-              style={[globalStyle, s.input]}
-              secureTextEntry={true}
-              placeholder='Wat is je wachtwoord?'
-              placeholderTextColor={white}
-            />
-          </View>
-          <View style={s.buttonContainer}>
-            <Button
-              style={s.button}
-              textStyle={[globalStyle, s.buttonText]}>
-              { action }
-            </Button>
-          </View>
+        <Text style={s.title}>{ title }</Text>
+        <View style={s.inputContainer}>
+          <TextInput
+            style={s.input}
+            placeholder='Wat is je e-mail adres?'
+            placeholderTextColor={white}
+          />
+        </View>
+        <View style={s.inputContainer}>
+          <TextInput
+            style={s.input}
+            secureTextEntry={true}
+            placeholder='Wat is je wachtwoord?'
+            placeholderTextColor={white}
+          />
+        </View>
+        <View style={s.buttonContainer}>
+          <Button style={s.button} textStyle={s.buttonText}>{ action }</Button>
+        </View>
       </View>
       <View style={s.anonLogin}>
-        <Text style={[globalStyle, s.underlineLink]}>Inloggen zonder account</Text>
+        <Text style={s.underlineLink}>Inloggen zonder account</Text>
       </View>
     </View>
   </LinearGradient>
