743db476dd9a848197d5be1aeaf3c7f0cc1b3dae
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-02-02 12:13:05 +0100

    :lipstick: Added UserPicker container + style

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 
+import UserPicker from 'src/containers/UserPicker';
 import Dashboard from 'src/containers/Dashboard';
 
 const App = () => (
-  <Router>
-    <Scene key='rootScene' hideNavBar={true}>
+  <Router hideNavBar={true}>
+    <Scene key='rootScene' component={UserPicker} />
+    <Scene key='dashboardScenes'>
       <Scene key='dashboard' component={Dashboard} title='Dashboard' initial={true} />
     </Scene>
   </Router>
