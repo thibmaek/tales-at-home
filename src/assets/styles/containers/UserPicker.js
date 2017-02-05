75914910851ca53ec74a42177107e8736a54b79a
Jonas Leupe <jonas.leupe@student.howest.be>
2017-02-05 14:27:37 +0100

    :lipstick: updated UI UserPicker

--- src/assets/styles/containers/UserPicker.js
+++ src/assets/styles/containers/UserPicker.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import { lightBlue } from 'src/assets/styles/vars';
+import { accentYellow, lightBlue } from 'src/assets/styles/vars';
 
 const sharedContainerRules = {
   flex: 1, justifyContent: `space-around`, flexDirection: `row`, alignItems: `center`,
@@ -9,15 +9,34 @@ const sharedContainerRules = {
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: lightBlue,
   },
   buttonsContainer: {
     ...sharedContainerRules,
-    alignItems: `center`,
-    justifyContent: `space-around`,
+  },
+  professionalContainer: {
+    flex: 1, justifyContent: `flex-end`,
+    minHeight: 768,
+    backgroundColor: lightBlue,
+  },
+  professionalInnerContainer: {
+    flexDirection: `column`, alignItems: `center`, justifyContent: `center`,
+  },
+  professionalChar: {
+    marginBottom: 30,
+  },
+  familyContainer: {
+    flex: 1, justifyContent: `flex-end`,
+    minHeight: 768,
+    backgroundColor: accentYellow,
+  },
+  familyInnerContainer: {
+    flexDirection: `column`, alignItems: `center`, justifyContent: `center`,
   },
   charsContainer: {
     ...sharedContainerRules,
     alignItems: `flex-start`,
+  },
+  button: {
+    marginBottom: 80, marginTop: 80,
   },
 });
