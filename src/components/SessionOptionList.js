c64592b9058910d34f2c019e382544fa2a715c15
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-02-06 13:04:37 +0100

    :bug: Sessions are now displayed ActionButton will only render them if they're passed. Fixed some other bugs

--- src/components/SessionOptionList.js
+++ src/components/SessionOptionList.js
@@ -1,28 +1,33 @@
 import React, { PropTypes } from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import s from 'src/assets/styles/components/SessionOptionList';
 import cross from 'src/assets/img/icons/cross@2x.png';
 
 import { SessionOption } from 'src/components/';
 
-const SessionOptions = () => (
-  <View>
-    <View style={s.sessionOptionContainer}>
-      <View style={s.headerBar}>
-        <Text style={s.title}>Nieuwe sessie starten</Text>
-        <Image style={s.cross} source={cross} />
-      </View>
-      <View style={s.contentContainer}>
-        {this.props.sessionOptions.map(session =>
-          <SessionOption key={session.key} {...session} />)}
+const SessionOptionList = ({ sessionOptions, onClose }) => {
+  console.log(onClose);
+  return (
+    <View>
+      <View style={s.sessionOptionContainer}>
+        <View style={s.headerBar}>
+          <Text style={s.title}>Nieuwe sessie starten</Text>
+          <TouchableOpacity onPress={onClose}>
+            <Image style={s.cross} source={cross} />
+          </TouchableOpacity>
+        </View>
+        <View style={s.contentContainer}>
+          {sessionOptions.map(session => <SessionOption key={session.key} {...session} />)}
+        </View>
       </View>
     </View>
-  </View>
-);
-
-SessionOptions.propTypes = {
-  sessionOptions: PropTypes.array,
+  );
 };
 
-export default SessionOptions;
+SessionOptionList.propTypes = {
+  sessionOptions: PropTypes.array.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default SessionOptionList;
