c1f55aa0efd85e2181dabd0cd66b6ae4db26ba17
Anthony Magnus <anthony.magnus@student.howest.be>
2017-02-09 13:22:24 +0100

    Need help

--- src/containers/Families.js
+++ src/containers/Families.js
@@ -1,9 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { ListView, TouchableHighlight, View, TextInput } from 'react-native';
+import { ListView, TouchableHighlight } from 'react-native';
 
 import { FamilyItem } from 'src/components/';
-
-import s from 'src/assets/styles/containers/Dashboard';
 
 import { highLightNeutral } from 'src/assets/styles/vars';
 
@@ -19,47 +17,25 @@ export default class Families extends Component {
     this.setState({ active });
   }
 
-  _updateSearch(e) {
-    const searchText = e.nativeEvent.text;
-    this.setState({ searchText });
-
-    const filteredData  = this.state.active.filter(
-      family => {
-        return family.name.indexOf(e.nativeEvent.text) !== - 1;
-      }
-    );
-    //
-    this.setState({
-      active: this.ds.cloneWithRows(filteredData),
-    });
-  }
-
   shouldHighlight = key => key === this.props.selectedFamily ? true : false
 
   render() {
     return (
-      <View>
-        <View style={s.searchContainer}>
-          <TextInput style={s.input} placeholder='Zoek een gezin'
-            value={this.state.searchText}
-            onChange={e => this._updateSearch(e)} />
-        </View>
-        <ListView
-          dataSource={this.ds.cloneWithRows(this.state.active)}
-          renderRow={family => (
-            <TouchableHighlight
-              onPress={() => this.props.didSelectFamily(family.key)}
-              underlayColor={highLightNeutral}
-            >
-              <FamilyItem
-                key={family.key}
-                shouldHighlight={this.shouldHighlight(family.key)}
-                {...family}
-              />
-            </TouchableHighlight>
-          )}
-        />
-    </View>
+      <ListView
+        dataSource={this.ds.cloneWithRows(this.state.active)}
+        renderRow={family => (
+          <TouchableHighlight
+            onPress={() => this.props.didSelectFamily(family.key)}
+            underlayColor={highLightNeutral}
+          >
+            <FamilyItem
+              key={family.key}
+              shouldHighlight={this.shouldHighlight(family.key)}
+              {...family}
+            />
+          </TouchableHighlight>
+        )}
+      />
     );
   }
 
