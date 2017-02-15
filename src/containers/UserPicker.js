11d41feca5673aa661ee4340ec316545b5fec7f4
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-02-15 12:52:43 +0100

    :hankey: demo

--- src/containers/UserPicker.js
+++ src/containers/UserPicker.js
@@ -1,43 +1,56 @@
 import React from 'react';
-import { StatusBar, View, TouchableWithoutFeedback, Image } from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import DynamicButton from 'rndynamicbutton';
+import Video from 'react-native-video';
 
-import s from 'src/assets/styles/containers/UserPicker';
-import professionalChar from 'src/assets/img/chars/professionalChar.png';
-import familyChars from 'src/assets/img/chars/familyChars.png';
+const VideoPlayer = () => {
+  return (
+    <Video repeat resizeMode='cover'
+    source={require(`src/assets/video/playground.mp4`)}
+    style={{ width: 1024, height: 768 }}
+  />
+  );
+};
 
-const AUTH_REGISTER = { action: `registreren`, title: `registreren`, method: `register` };
+export default VideoPlayer;
 
-const UserPicker = () => (
-  <View style={s.container}>
-    <StatusBar hidden={true} />
-    <View style={s.buttonsContainer}>
-      <View style={s.professionalContainer}>
-        <TouchableWithoutFeedback onPress={() => Actions.setupScene({ authType: AUTH_REGISTER })}>
-          <View style={s.professionalInnerContainer}>
-            <Image source={professionalChar} style={s.char} />
-            <DynamicButton touchable='highlight' style={s.button} textStyle={s.buttonText}
-              action={() => Actions.setupScene({ authType: AUTH_REGISTER })}>
-              Ik ben een professional
-            </DynamicButton>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-      <View style={s.familyContainer}>
-        <TouchableWithoutFeedback onPress={() => Actions.connectionScene}>
-          <View style={s.familyInnerContainer}>
-            <Image source={familyChars} style={s.char} />
-            <DynamicButton touchable='highlight' style={s.button}
-              textStyle={s.buttonText} action={() => Actions.connectionScene()}
-            >
-              Ik ben een gezinslid
-            </DynamicButton>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    </View>
-  </View>
-);
-
-export default UserPicker;
+// import { StatusBar, View, TouchableWithoutFeedback, Image } from 'react-native';
+// import { Actions } from 'react-native-router-flux';
+// import DynamicButton from 'rndynamicbutton';
+//
+// import s from 'src/assets/styles/containers/UserPicker';
+// import professionalChar from 'src/assets/img/chars/professionalChar.png';
+// import familyChars from 'src/assets/img/chars/familyChars.png';
+//
+// const AUTH_REGISTER = { action: `registreren`, title: `registreren`, method: `register` };
+//
+// const UserPicker = () => (
+//   <View style={s.container}>
+//     <StatusBar hidden={true} />
+//     <View style={s.buttonsContainer}>
+//       <View style={s.professionalContainer}>
+//         <TouchableWithoutFeedback onPress={() => Actions.setupScene({ authType: AUTH_REGISTER })}>
+//           <View style={s.professionalInnerContainer}>
+//             <Image source={professionalChar} style={s.char} />
+//             <DynamicButton touchable='highlight' style={s.button} textStyle={s.buttonText}
+//               action={() => Actions.setupScene({ authType: AUTH_REGISTER })}>
+//               Ik ben een professional
+//             </DynamicButton>
+//           </View>
+//         </TouchableWithoutFeedback>
+//       </View>
+//       <View style={s.familyContainer}>
+//         <TouchableWithoutFeedback onPress={() => Actions.connectionScene}>
+//           <View style={s.familyInnerContainer}>
+//             <Image source={familyChars} style={s.char} />
+//             <DynamicButton touchable='highlight' style={s.button}
+//               textStyle={s.buttonText} action={() => Actions.connectionScene()}
+//             >
+//               Ik ben een gezinslid
+//             </DynamicButton>
+//           </View>
+//         </TouchableWithoutFeedback>
+//       </View>
+//     </View>
+//   </View>
+// );
+//
+// export default UserPicker;
